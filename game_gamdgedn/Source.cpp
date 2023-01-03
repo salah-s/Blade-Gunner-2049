53ee32af248e2482d5fd22db95c0b279af9a5151
salah mohamed <salah.m.s2003@gmail.com>
2023-01-04 00:42:09 +0200

    .

--- game_gamdgedn/Source.cpp
+++ game_gamdgedn/Source.cpp
@@ -1,40 +1,58 @@
 #include "raylib.h"
 #define screenheight 450
 #define screenwidth 800
-//test
-void main() {
 
+void main() {
+	//initializing window
  InitWindow(screenwidth,screenheight,"game title");
+ SetTargetFPS(60);
+
+ // loading textures
+ Texture2D playerr;
+ playerr = LoadTexture("resources/p1r.png");
+
+ Texture2D playerl;
+ playerl = LoadTexture("resources/p1l.png");
 
  Texture2D player;
- player = LoadTexture("resources/fawzy.gif");
- 
- //Texture2D bg;
- //bg = LoadTexture("resources/zarya.png");
+ player = LoadTexture("resources/p1r.png");
 
- Vector2 characterpos = { 0, 0 };
+ Texture2D bg;
+ bg = LoadTexture("resources/bg.png");
+
  
- SetTargetFPS(60);
+ Vector2 characterpos = { 0, 0 };
+ int playerspeed = 3;
+
+ // GAME LOOP
  while (!WindowShouldClose()) {
 
+	 //character movement
+	 if (IsKeyDown(KEY_RIGHT)) {
+		 characterpos.x += playerspeed;	
+		 player = playerr;       //changing texture to facing right
+	 }
+	 if (IsKeyDown(KEY_LEFT)) {
+		 characterpos.x -= playerspeed;
+		 player = playerl; 		 //changing texture to facing right
+	 }
+	 if (IsKeyDown(KEY_UP))
+		 characterpos.y -= playerspeed;
+	 if (IsKeyDown(KEY_DOWN))
+		 characterpos.y += playerspeed;
+
+	 
+
+	 //drawing
 	 BeginDrawing();
+
 	 ClearBackground(WHITE);
 
-	 //DrawTexture(bg,0, 0, WHITE);
+	 DrawTexture(bg,0, 0, WHITE);
 
-	 DrawTexture(player,characterpos.x,characterpos.y,WHITE);
+		DrawTexture(player, characterpos.x, characterpos.y, WHITE);  
 
 	 EndDrawing();
-
-	  if (IsKeyDown(KEY_RIGHT))
-		 characterpos.x += 3;
-	 if (IsKeyDown(KEY_LEFT))
-		 characterpos.x -= 3; 
-	 if (IsKeyDown(KEY_UP))
-		 characterpos.y -= 3;
-	 if (IsKeyDown(KEY_DOWN))
-		 characterpos.y += 3;
-		 
 
 
 
