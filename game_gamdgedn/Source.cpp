d7b66698968e04ae9365f7c4db44a3f25210a930
StuffOmarMade <work.omarkhaled@gmail.com>
2023-01-04 17:34:08 +0200

    updated player texture turning (left and right)mechanism

--- game_gamdgedn/Source.cpp
+++ game_gamdgedn/Source.cpp
@@ -1,79 +1,78 @@
 #include "raylib.h"
-#define screenheight 720
-#define screenwidth 1280
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 void main() {
 	//initializing window
- InitWindow(screenwidth,screenheight,"game title");
- SetTargetFPS(60);
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game title"); 
+	SetTargetFPS(60);
 
- // loading textures
- Texture2D playerr;
- playerr = LoadTexture("resources/p1r.png");
+	// loading textures
+	Texture2D playerr;
+	playerr = LoadTexture("resources/p1r.png");
 
- Texture2D playerl;
- playerl = LoadTexture("resources/p1l.png");
+	Texture2D playerl;
+	playerl = LoadTexture("resources/p1l.png");
 
- Texture2D player;
- player = LoadTexture("resources/p1r.png");
+	Texture2D player;
+	player = LoadTexture("resources/p1r.png");
 
- Texture2D bg;
- bg = LoadTexture("resources/bg.png");
-
+	Texture2D bg;
+	bg = LoadTexture("resources/bg.png");
  
- Vector2 characterpos = { 0, 0 };
-  int playerspeed = 3;
-
-  //initializing camera
-  Camera2D playercam = {0};
-  playercam.target = characterpos;
-  playercam.offset = { screenwidth / 2.0f, screenheight / 2.0f };
-  playercam.rotation = 0.0f;
-  playercam.zoom = 1.4f;
-
- // GAME LOOP
- while (!WindowShouldClose()) {
-
-	 //enabling fullscreen mode
-	 if (IsKeyPressed(KEY_F))
-		 ToggleFullscreen();
-
-	 playercam.target = { characterpos.x + 20, characterpos.y + 20 };
-
-	 //character movement
-	 if (IsKeyDown(KEY_RIGHT)) {
-		 characterpos.x += playerspeed;	
-		 player = playerr;       //changing texture to facing right
-	 }
-	 if (IsKeyDown(KEY_LEFT)) {
-		 characterpos.x -= playerspeed;
-		 player = playerl; 		 //changing texture to facing right
-	 }
-	 if (IsKeyDown(KEY_UP))
-		 characterpos.y -= playerspeed;
-	 if (IsKeyDown(KEY_DOWN))
-		 characterpos.y += playerspeed;
-
-
-
-		
-
-	 //drawing
-	 BeginDrawing();
-
-	 BeginMode2D(playercam); //showing camera
+	Vector2 characterPos = { 0, 0 };
+	int playerspeed = 3;
 	
-	 ClearBackground(WHITE);
+	Rectangle src;
+	src.width = player.width;
+	src.height = player.height;
+	src.x = characterPos.x;
+	src.y = characterPos.y;
 
-	 DrawTexture(bg,0, 0, WHITE);
+	//initializing camera
+	Camera2D playerCam = {0};
+  
+	playerCam.target = characterPos;
+	playerCam.offset = { (SCREEN_WIDTH / 2.0f) - (player.width / 2), (SCREEN_HEIGHT / 2.0f) - (player.height / 2) };
+	playerCam.rotation = 0.0f;
+	playerCam.zoom = 1.4f;
 
-		DrawTexture(player, characterpos.x, characterpos.y, WHITE);  
+	// GAME LOOP
+	while (!WindowShouldClose()) {
+		
+		//enabling fullscreen mode
+		if (IsKeyPressed(KEY_F))
+			ToggleFullscreen();
 
-	 EndDrawing();
+		playerCam.target = { characterPos.x + 20, characterPos.y + 20 };
 
+		//character movement
+		if (IsKeyDown(KEY_RIGHT)) {
+			characterPos.x += playerspeed;	
+			src.width = player.width; 	      //changing texture to facing right
+		}
+		
+		if (IsKeyDown(KEY_LEFT)) {
+			characterPos.x -= playerspeed;
+			src.width = -player.width; 		 //changing texture to facing right
+		}
+		
+		if (IsKeyDown(KEY_UP))
+			characterPos.y -= playerspeed;
+		if (IsKeyDown(KEY_DOWN))
+			characterPos.y += playerspeed;
 
+		//drawing
+		BeginDrawing();
+			BeginMode2D(playerCam); //showing camera
+	
+			ClearBackground(WHITE);
 
- }
+			DrawTexture(bg,0, 0, WHITE);
 
+			DrawTextureRec(player, src,characterPos, WHITE);  
 
+		EndDrawing();
+	 }
 }
