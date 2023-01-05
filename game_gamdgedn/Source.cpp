86fb6d64fbccbc39dc6af154282500a4702fc838
salah mohamed <salah.m.s2003@gmail.com>
2023-01-06 01:26:07 +0200

    now enemies get spawned randomly and follow the player problems that we need to solve: 1)enemies spawns around the player's startting position instead of his current position 2)only the first spawned enemy is following the player correctly ,the other enemies are copying the movement of the first enemy instead of following the player indiviually 3)all enemies dissapear when the first enemy touches the player

--- game_gamdgedn/Source.cpp
+++ game_gamdgedn/Source.cpp
@@ -1,70 +1,78 @@
 #include "raylib.h"
-#include <stdlib.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+#define max_enemies 200
 
-struct zombie {
+//this function is not working 100% correctly: (it only spawns enemies at the starting position of the player,its not following the player position)
+// a function that generates a random number between a min dist from the player and a max dist from the player
+float GetRandomSafeValue(float playerposx, float mindis, float maxdis) {
+	int x;
+
+	for (int i = 0;i < 500;i++) {
+		x = GetRandomValue(-maxdis, maxdis);
+		if ((x< playerposx + maxdis && x>playerposx + mindis) || x > playerposx - maxdis && x < playerposx - mindis)
+			return x;
+	}
+}
+
+
+struct Enemy { //making enemies struct 
 	Texture2D texture;
 	Vector2 position;
-	int speed;
-
+	Vector2 direction;
+	int hyp;
+	bool active;
 };
 
 
 void main() {
 	//initializing window
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game title"); 
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game title");
 	SetTargetFPS(60);
+
+
 
 	// loading textures
 	Texture2D player = LoadTexture("resources/p1r.png");
 
-	Texture2D texzombie = LoadTexture("resources/p1l.png");
+	Texture2D enemytex = LoadTexture("resources/mob.png");
 
-	Texture2D bg;
-	bg = LoadTexture("resources/bg.png");
- 
-zombie *zombies = (zombie*) malloc(100*sizeof(zombie));
+	Texture2D bg = LoadTexture("resources/bg.png");
 
-//vectors
+
+	//vectors
 	Vector2 playerpos = { 0, 0 };
-	Vector2 enemypos = { GetRandomValue(100,300),GetRandomValue(100,300)};
+	Vector2 enemypos;
 
 	//variables
 	int playerspeed = 3;
 	int enemyspeed = 2;
-	float timer = 0.0f;
-int zombiecount =0;
-	
-	Rectangle src; //player rec
-	src.width = player.width;
-	src.height = player.height;
-	src.x = playerpos.x;
-	src.y = playerpos.y;
+	int etimer = 0;
+	int enemyspawnrate = 3;
 
 	//initializing camera
-	Camera2D playerCam = {0};
+	Camera2D playerCam = { 0 };
 	playerCam.target = playerpos;
 	playerCam.offset = { (SCREEN_WIDTH / 2.0f) - (player.width / 2), (SCREEN_HEIGHT / 2.0f) - (player.height / 2) };
 	playerCam.rotation = 0.0f;
 	playerCam.zoom = 1.4f;
 
+	//making an array of "Enemy(s)"
+	struct Enemy e[max_enemies] = { 0 };
+
+
+	//intitalizing enemies
+	for (int i = 0; i < max_enemies; i++)
+	{
+
+		e[i].position = { Vector2{0,0} };
+		e[i].active = false;
+	}
+
 	// GAME LOOP
 	while (!WindowShouldClose()) {
-
-// Create more zombies
-            for (int i = 0; i < 100; i++)
-            {
-                if (zombie count < 50)
-                {
-                    zombies[zombiecount].position = (float)GetRandomValue(100,300);
-                    zombies[bunniesCount].speed.x = 0/60.0f;
-                    zombies[zombiecount].speed.y = 0/60.0f;
-                    zombiecount++;
-                }
-            }
-        }
 
 		//enabling fullscreen mode
 		if (IsKeyPressed(KEY_F))
@@ -75,12 +83,12 @@ int zombiecount =0;
 		//character movement
 		if (IsKeyDown(KEY_RIGHT)) {
 			playerpos.x += playerspeed;
-			src.width = player.width; 	      //changing texture to facing right
+
 		}
 
 		if (IsKeyDown(KEY_LEFT)) {
 			playerpos.x -= playerspeed;
-			src.width = -player.width; 		 //changing texture to facing right
+
 		}
 
 		if (IsKeyDown(KEY_UP))
@@ -88,11 +96,43 @@ int zombiecount =0;
 		if (IsKeyDown(KEY_DOWN))
 			playerpos.y += playerspeed;
 
-		//spawn enemies
-		timer += GetFrameTime();
+		//activating enemies and giving them positions
+		etimer++;
+		for (int i = 0;i < max_enemies;i++) {
 
-			//drawing
-			BeginDrawing();
+			if (!e[i].active && etimer % (60 / enemyspawnrate) == 0) {
+				e[i].active = true;
+				e[i].position.x = GetRandomSafeValue(playerpos.x, 100, 200);
+				e[i].position.y = GetRandomSafeValue(playerpos.y, 100, 200);
+
+				break;
+			}
+		}
+
+
+		//update enemies
+		for (int i = 0;i < max_enemies;i++) {
+
+			if (e[i].active) {
+				//enemy direction to follow player = player position - enemy position 
+				e[i].direction.x = (playerpos.x - e[i].position.x);
+				e[i].direction.y = (playerpos.y - e[i].position.y);
+
+				//normalizing the direction vector
+				e[i].hyp = sqrt(e[i].direction.x * e[i].direction.x + e[i].direction.y * e[i].direction.y);
+				e[i].direction.x /= e[i].hyp;
+				e[i].direction.y /= e[i].hyp;
+
+				// update enemy position (movement)
+				e[i].position.x += e[1].direction.x * 1;
+				e[i].position.y += e[1].direction.y * 1;
+			}
+		}
+
+
+
+		//drawing
+		BeginDrawing();
 		BeginMode2D(playerCam); //showing camera
 
 		ClearBackground(WHITE);
@@ -100,14 +140,19 @@ int zombiecount =0;
 		DrawTexture(bg, 0, 0, WHITE);
 
 		DrawTexture(player, playerpos.x, playerpos.y, WHITE);
-		if (timer>5.0f) {
-		
-		enemystruct enemy1;
-		enemy1.position = { (float)GetRandomValue(100,300),(float)GetRandomValue(100,300) };
-		enemy1.texture = 
-			DrawTexture(enemy1, enemypos.x, enemypos.y, WHITE);
-		
-	}
+
+
+
+		//drawing active enemies
+
+		for (int i = 0; i < max_enemies; i++)
+		{
+			if (e[i].active)
+				DrawTexture(enemytex, e[i].position.x, e[i].position.y, WHITE);
+		}
+
+
+
 		EndDrawing();
-	 }
+	}
 }
