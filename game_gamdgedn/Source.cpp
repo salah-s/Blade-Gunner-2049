9c473bd6b5b4023ed31e6a279560125268158641
salah mohamed <salah.m.s2003@gmail.com>
2023-01-07 13:34:36 +0200

    added shooting bullets (not working)

--- game_gamdgedn/Source.cpp
+++ game_gamdgedn/Source.cpp
@@ -5,6 +5,7 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define MAX_ENEMIES 200
+#define MAX_BULLETS 20
 
 // A function that generates a random number between a min dist from the player and a max dist from the player
 float GetRandomSafeValue(float playerPos, float minDist, float maxDist) {
@@ -58,7 +59,16 @@ struct Enemy {
 	Rectangle EnemyHitbox;
 	int hyp;
 	bool active;
+	int enemySpeed;
 	
+};
+struct Bullet {
+	Texture2D texture;
+	Vector2 position;
+	Vector2 direction;
+	Rectangle BulletHitbox;
+	bool active;
+	int bulletspeed;
 };
 
 void main() {
@@ -77,12 +87,14 @@ void main() {
 	Vector2 enemyPos;
 
 	// Rectangles
-	Rectangle PlayerHitbox = { 00,00,20,45};
+	Rectangle PlayerHitbox = { 00,00,20,45 };
 
 	// Variables
 	int playerSpeed = 3;
 	int playerHealth = 10;
-	int enemySpeed = 2;
+
+
+
 	int enemyTimer = 0;
 	float enemySpawnRate = 1.0f;
 
@@ -91,10 +103,7 @@ void main() {
 	bool collided = false;
 	bool gameOver = false;
 
-	Timer flashingTimer;
-	flashingTimer.Lifetime = 0.0f;
-
-	// Initializing camera
+	// Initiallizing camera
 	Camera2D playerCam = { 0 };
 
 	playerCam.target = playerPos;
@@ -102,17 +111,34 @@ void main() {
 	playerCam.rotation = 0.0f;
 	playerCam.zoom = 1.4f;
 
-	// An array of "Enemy(s)"
-	Enemy enemies[MAX_ENEMIES] = { 0 };
+	// Initiallizing enemies
 
-	// Intitalizing enemies
+	Enemy enemies[MAX_ENEMIES] = { 0 };     // An array of "Enemy(s)"
+
 	for (int i = 0; i < MAX_ENEMIES; i++)
 	{
 		enemies[i].position = { Vector2{0,0} };
 		enemies[i].active = false;
 		enemies[i].EnemyHitbox.width = 26;
 		enemies[i].EnemyHitbox.height = 26;
+		enemies[i].enemySpeed = 2;
 	}
+
+	// intializing bullets
+	Bullet bullets[MAX_BULLETS] = { 0 };   // An array of "Bullet(s)"
+	for (int i = 0; i < MAX_BULLETS; i++) {
+		bullets[i].position = Vector2{ 0,0 };
+		bullets[i].active = false;
+		bullets[i].BulletHitbox.width = 5;
+		bullets[i].BulletHitbox.height = 5;
+		bullets[i].bulletspeed = 4;
+
+	}
+
+
+	//initiallizing timers
+	Timer flashingTimer;
+	flashingTimer.Lifetime = 0.0f;
 
 	// GAME LOOP
 	while (!WindowShouldClose()) {
@@ -169,9 +195,10 @@ void main() {
 
 					// Normalizing the direction vector (to move with the same speed diagonally)
 					enemies[i].hyp = sqrt(enemies[i].direction.x * enemies[i].direction.x + enemies[i].direction.y * enemies[i].direction.y);
-					enemies[i].direction.x /= enemies[i].hyp;
-					enemies[i].direction.y /= enemies[i].hyp;
-
+					if (enemies[i].hyp != 0) {
+						enemies[i].direction.x /= enemies[i].hyp;
+						enemies[i].direction.y /= enemies[i].hyp;
+					}
 					// Update the enemy's position (movement)
 					enemies[i].position.x += enemies[i].direction.x * 1;
 					enemies[i].position.y += enemies[i].direction.y * 1;
@@ -186,10 +213,9 @@ void main() {
 						playerHealth--;
 						StartTimer(&flashingTimer, flashingDuration);
 					}
-					
+
 				}
 			}
-
 			UpdateTimer(&flashingTimer);
 
 			if (collided && isTimerDone(&flashingTimer))
@@ -199,41 +225,76 @@ void main() {
 				gameOver = true;
 		}
 
-		// DRAWING
-		BeginDrawing();
-		BeginMode2D(playerCam); // Showing camera
-
-		ClearBackground(WHITE);
-
-		DrawTexture(bg, 0, 0, WHITE);
-		DrawTexture(playerTex, playerPos.x, playerPos.y, WHITE);
-
-		//DrawRectangleRec(PlayerHitbox, BLUE);
-
-		// Drawing active enemies
-		for (int i = 0; i < MAX_ENEMIES; i++)
-		{
-			if (enemies[i].active) {
-				DrawTexture(enemyTex, enemies[i].position.x, enemies[i].position.y, WHITE);
-				//DrawRectangleRec(enemies[i].EnemyHitbox, RED);  
+		//activate bullets
+		for (int i = 0;i < MAX_BULLETS;i++) {
+			if (!bullets[i].active && IsKeyPressed(MOUSE_BUTTON_LEFT)) {
+				bullets[i].active = true;
+				bullets[i].position = playerPos;
+				bullets[i].BulletHitbox.x = bullets[i].position.x;
+				bullets[i].BulletHitbox.y = bullets[i].position.y;
+			break;
 			}
 		}
 
-		DrawText(TextFormat("Player's Health: %d", playerHealth), 10, 10, 24, BLACK);
+		//update bullets
+		for (int i = 0;i < MAX_BULLETS;i++) {
+			if (bullets[i].active) {
+				bullets[i].direction.x = GetMousePosition().x - playerPos.x;
+				bullets[i].direction.y = GetMousePosition().y - playerPos.y;
 
-		if (gameOver)
-			DrawText("GAME OVER", 100, 100, 42, BLACK);
+				bullets[i].position.x += bullets[i].direction.x * bullets[i].bulletspeed;
+				bullets[i].position.y += bullets[i].direction.y * bullets[i].bulletspeed;
+			}
 
-		if (collided)
-			DrawText("colision", 50, 50, 42, BLACK);
-		else
-			DrawText("no colision", 50, 50, 42, BLACK);
-		if (!(flashingTimer.Lifetime <= 0.0))
-			DrawText("timer is running!", 50, 10, 42, BLACK);
-		else
-			DrawText("timer is not running", 50, 10, 42, BLACK);
 
-		EndDrawing();
 
+			// DRAWING
+			BeginDrawing();
+			BeginMode2D(playerCam); // Showing camera
+
+			ClearBackground(WHITE);
+
+			DrawTexture(bg, 0, 0, WHITE);
+			DrawTexture(playerTex, playerPos.x, playerPos.y, WHITE);
+
+			//DrawRectangleRec(PlayerHitbox, BLUE);
+
+			// Drawing active enemies
+			for (int i = 0; i < MAX_ENEMIES; i++)
+			{
+				if (enemies[i].active) {
+					DrawTexture(enemyTex, enemies[i].position.x, enemies[i].position.y, WHITE);
+					//DrawRectangleRec(enemies[i].EnemyHitbox, RED);  
+				}
+			}
+			//draw bullets
+			for (int i = 0; i < MAX_BULLETS; i++)
+			{
+				if (bullets[i].active) {
+					DrawRectangleRec(bullets[i].BulletHitbox, RED);
+
+				}
+			}
+
+			//DrawRectangleRec(enemies[i].EnemyHitbox, RED);  
+
+			//other drawings
+			DrawText(TextFormat("Player's Health: %d", playerHealth), 10, 10, 24, BLACK);
+
+			if (gameOver)
+				DrawText("GAME OVER", 100, 100, 42, BLACK);
+
+			if (collided)
+				DrawText("colision", 50, 50, 42, BLACK);
+			else
+				DrawText("no colision", 50, 50, 42, BLACK);
+			if (!(flashingTimer.Lifetime <= 0.0))
+				DrawText("timer is running!", 50, 10, 42, BLACK);
+			else
+				DrawText("timer is not running", 50, 10, 42, BLACK);
+
+			EndDrawing();
+
+		}
 	}
 }
